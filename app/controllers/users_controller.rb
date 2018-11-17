d621c5df06972b97bb25362877ebd49fcabf4286
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-17 16:35:08 -0700

    user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action
+  before_action :set_user, only: [:show, :destroy]
   def index
     @users = User.all
   end
@@ -8,9 +8,16 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
   end
 
   def edit
@@ -26,6 +33,9 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def user_params
   end
   
 end
