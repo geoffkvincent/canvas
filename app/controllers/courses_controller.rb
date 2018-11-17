6db30d196573d411e635070003b838c57add9e13
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-17 16:14:06 -0700

    user controller

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,10 @@
 class CoursesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,31 @@
 class UsersController < ApplicationController
+  before_action
+  def index
+    @users = User.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
   
 end
