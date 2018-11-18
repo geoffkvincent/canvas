cec509389a3b2df41ea8900f12636ee30a80c24e
Geoff Vincent <geoffkvincent@gmail.com>
2018-11-17 20:57:42 -0700

    course views

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,52 @@
 class CoursesController < ApplicationController
+  before_action :set_course, only: [:show, :destroy]
+
   def index
+    @courses = Course.all
   end
 
   def show
   end
 
   def new
+    @course = Course.new
+    render partial: 'form'
+  end
+
+  def create
+    @course = Course.new(course_params)
+
+    if @course.save
+      redirect_to courses_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+    render partial: 'form'
+  end
+
+  def update
+    if course.update(course_params)
+      redirect_to @course
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @course.destroy
+    redircet_to courses_path
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:name)
   end
 end
